8746e1d512bcdbb66085eb2347f18741116e8953
apex2801 <aapinheiro2005@gmail.com>
2021-03-14 20:17:54 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nome_completo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
